4ed6464f47e5c002fd6e1339af66a765487257bb
Piotr Szafarczyk <dev@netexpert.pl>
2025-09-03 21:03:41 +0200

    refactor: rename Database->Db, Configuration->Config

--- tests/dummy_database.py
+++ tests/dummy_database.py
@@ -1,13 +1,13 @@
 from password_admin.auth import LoginCredentials
 from password_admin.auth import NewCredentials
-from password_admin.database.configuration import DatabaseConnectionConfiguration
-from password_admin.exceptions import DatabaseConnectionError
-from password_admin.exceptions import DatabaseLoginError
-from password_admin.exceptions import DatabaseQueryError
+from password_admin.database.config import DbConfig
+from password_admin.exceptions import DbConnectionError
+from password_admin.exceptions import DbLoginError
+from password_admin.exceptions import DbQueryError
 
 
-class DummyDatabaseConfiguration(DatabaseConnectionConfiguration):
-    """Configuration for DummyDatabase."""
+class DummyDbConfig(DbConfig):
+    """Configuration for DummyDbConnection."""
 
     number_of_users: int = 1
     login_successful: bool = True
@@ -19,36 +19,36 @@ class DummyDatabaseConfiguration(DatabaseConnectionConfiguration):
     set_password_query_problem: bool = False
 
 
-class DummyDatabase:
+class DummyDbConnection:
     """Database for tests."""
 
-    def __init__(self, configuration: DummyDatabaseConfiguration):
+    def __init__(self, configuration: DummyDbConfig):
         self.__configuration = configuration
         self.logged_in = False
         self.password_changed = False
 
     def login(self, credentials: LoginCredentials) -> None:
         if self.__configuration.login_connection_problem:
-            raise DatabaseConnectionError
+            raise DbConnectionError
         if not self.__configuration.login_successful:
-            raise DatabaseLoginError(username=credentials.username)
+            raise DbLoginError(username=credentials.username)
         self.logged_in = True
 
     def logout(self) -> None:
         if self.__configuration.logout_connection_problem:
-            raise DatabaseConnectionError
+            raise DbConnectionError
         self.logged_in = False
 
     def get_users(self) -> list[str]:
         if self.__configuration.get_users_connection_problem:
-            raise DatabaseConnectionError
+            raise DbConnectionError
         if self.__configuration.get_users_query_problem:
-            raise DatabaseQueryError
+            raise DbQueryError
         return ['user'] * self.__configuration.number_of_users
 
     def set_password(self, credentials: NewCredentials) -> None:  # noqa: ARG002
         if self.__configuration.set_password_connection_problem:
-            raise DatabaseConnectionError
+            raise DbConnectionError
         if self.__configuration.set_password_query_problem:
-            raise DatabaseQueryError
+            raise DbQueryError
         self.password_changed = True
