61d80cf8d06cc00745f057de32497088f51566e7
Piotr Szafarczyk <dev@netexpert.pl>
2025-09-03 20:10:22 +0200

    feat: add DatabaseQueryError

--- src/password_admin/exceptions.py
+++ src/password_admin/exceptions.py
@@ -9,12 +9,19 @@ class BaseError(HTTPException):
 class DatabaseError(BaseError):
     """Base class for database-related errors."""
 
+
+class DatabaseConnectionError(DatabaseError):
+    """Raised when a database connection cannot be established."""
+
     def __init__(self, status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail: str = 'Communication error') -> None:
         super().__init__(status_code=status_code, detail=detail)
 
 
-class DatabaseConnectionError(DatabaseError):
-    """Raised when a database connection cannot be established."""
+class DatabaseQueryError(DatabaseError):
+    """Raised when a database query failed."""
+
+    def __init__(self, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail: str = 'Internal error') -> None:
+        super().__init__(status_code=status_code, detail=detail)
 
 
 class DatabaseLoginError(DatabaseError):
